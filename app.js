99d636b21c97d67f846fc4dfcb7d89ee8b598fb0
David Mutua <mutuadavi@gmail.com>
2021-04-18 09:06:57 +0300

    feat(basics): Understand event binding

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
-// Create the gloabal Vue app
 const app = Vue.createApp({
   data() {
-    // data function always returns an Object
     return {
-      // All properties here can only be accessed in the area are Vue controlled
-      courseGoal: "Finish the course and learn vue!",
-      courseGoal1: "Finish Vue first then learn Nuxt!",
-      courseGoal2: "Master Vue and build amazing enterprise apps",
-      vueLink: "https://vuejs.org/",
+      counter: 0,
     };
-  },
-
-  // All methods live here
-  methods: {
-    outputGoal() {
-      const score = Math.random();
-
-      // `this` keyword represents the entire Vue 'app' instance's returned data
-      const decision = score < 0.5 ? this.courseGoal1 : this.courseGoal2;
-      return decision;
-    },
   },
 });
 
-// NB :: If we control and HTML element with Vue, we'll also control all child
-// elements of that element.
-app.mount("#user-goal");
+app.mount('#events');
