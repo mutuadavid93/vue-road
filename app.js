39ab3a8ad79c587ad8b1293c0c6dc2f552629e49
David Mutua <mutuadavi@gmail.com>
2021-04-18 09:31:01 +0300

    feat(basics): Work with events and arguments

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
+  },
+
+  methods: {
+    increment(number) {
+      this.counter = this.counter + number;
+    },
+    decrement: function (number) {
+      this.counter = this.counter - number;
+    },
+    setName(event, lastname) {
+      this.name = event.target.value + " " + lastname;
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
