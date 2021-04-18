a9d9623c4471781554462b3f51026003c4efb737
David Mutua <mutuadavi@gmail.com>
2021-04-18 08:22:23 +0300

    feat(basics): Intro to vue basics

---
+++ app.js
@@ -0,0 +1,14 @@
+// Create the gloabal Vue app
+const app = Vue.createApp({
+  data() {
+    // data function always returns an Object
+    return {
+      // All properties here can only be accessed in the area are Vue controlled
+      courseGoal: "Finish Vue first then learn Nuxt!",
+    };
+  },
+});
+
+// NB :: If we control and HTML element with Vue, we'll also control all child
+// elements of that element.
+app.mount("#user-goal");
