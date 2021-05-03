51da33e190a42d8958f839c2e43a461c80a9ed07
David Mutua <mutuadavi@gmail.com>
2021-05-03 09:25:57 +0300

    feat(basics): Introduce computed properties

--- app.js
+++ app.js
@@ -7,6 +7,22 @@ const app = Vue.createApp({
     };
   },
 
+  // Computed Properties ::
+  // - Similar to methods but Vue is aware of their dependencies and only
+  // re-executed if any of those dependencies change.
+  // 
+  // - Only use computed properties if you want to display something.
+  computed: {
+    // NB: these methods are used the same way as the properties inside 'data'
+    fullname() {
+      console.log("Running again!!");
+      if (this.name === "") return "";
+      return this.name + " " + "Ericksen";
+    },
+  },
+
+
+  // NB:: You can only bind events on methods not computed properties
   methods: {
     increment(number) {
       this.counter = this.counter + number;
@@ -25,9 +41,9 @@ const app = Vue.createApp({
     },
 
     outPutFullName() {
-      console.log("Running again!!");
-      if (this.name === "") return "";
-      return this.name + " " + "Ericksen";
+      // console.log("Running again!!");
+      // if (this.name === "") return "";
+      // return this.name + " " + "Ericksen";
     },
   },
 });
