1eba7e2d4d5aa29db9a14ecfa36e39d5d5e57de7
David Mutua <mutuadavi@gmail.com>
2021-05-10 08:43:59 +0300

    Rendering lists setup

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
 const app = Vue.createApp({
   data() {
-    return {
-      boxASelected: false,
-      boxBSelected: false,
-      boxCSelected: false,
-    };
-  },
-
-  methods: {
-    boxSelected(box) {
-      if (box === "A") {
-        // Toggle the behavior by negating it based on the current truthy value
-        this.boxASelected = !this.boxASelected;
-      } else if (box === "B") {
-        this.boxBSelected = !this.boxBSelected;
-      } else {
-        this.boxCSelected = !this.boxCSelected;
-      }
-    },
+    return { goals: [] };
   },
 });
 
-app.mount("#styling");
+app.mount('#user-goals');
