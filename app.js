e73c8c5c79deb0a4adff66a6c6280bf27bcce74e
David Mutua <mutuadavi@gmail.com>
2021-05-19 19:19:44 +0300

    feat(demo-monster-slayer-game): Update health bars

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
+// Non Vue controlled helper functions can live outside Vue App
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 const app = Vue.createApp({
   data() {
     return {
-      enteredGoal: "",
-      goals: [],
+      monsterHealth: 100,
+      playerHealth: 100,
     };
   },
   methods: {
-    addGoal() {
-      this.goals.push(this.enteredGoal);
+    attackMonster() {
+      // Deal a damage which is a random whole number between 5 and 12 i.e.
+      // Minimum and Maximum
+      this.monsterHealth -= getRandomValue(5, 12);
+
+      // Mimic monster retaliating when it's attacked
+      this.attackPlayer();
     },
-    removeGoal(index) {
-      this.goals.splice(index, 1);
+    attackPlayer() {
+      this.playerHealth -= getRandomValue(5, 18);
+    },
+  },
+  computed: {
+    monsterHealthBarStyles() {
+      return { width: this.monsterHealth + "%" };
+    },
+    playerHealthBarStyles() {
+      return { width: this.playerHealth + "%" };
     },
   },
 });
 
-app.mount("#user-goals");
+app.mount("#game");
