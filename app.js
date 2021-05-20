9809dc2522486e02b4adf28660df500fb588afe9
David Mutua <mutuadavi@gmail.com>
2021-05-20 08:41:22 +0300

    feat(demo-monster-slayer-game): Add game over screen

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
       monsterHealth: 100,
       playerHealth: 100,
       currentRound: 0,
+      winner: null,
     };
   },
   methods: {
@@ -50,6 +51,26 @@ const app = Vue.createApp({
     },
     mayUseSpecialAttack() {
       return this.currentRound % 3 !== 0;
+    },
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        // Draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // Player Lost
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        // Draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // Monster Lost
+        this.winner = "player";
+      }
     },
   },
 });
