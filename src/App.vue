f728726d8014b491d520821664fe2f5c279cb0cf
David Mutua <mutuadavi@gmail.com>
2021-05-25 11:04:24 +0300

    feat(provide-and-inject-properties): Demo  and  features

--- src/App.vue
+++ src/App.vue
@@ -1,131 +1,115 @@
 <template>
-  <section>
-    <header><h3>My Friends</h3></header>
-    <new-friend @add-contact="addContact"></new-friend>
-    <ul>
-      <friend-contact
-        v-for="friend in friends"
-        :id="friend.id"
-        :key="friend.id"
-        :name="friend.name"
-        :phone-number="friend.phone"
-        :email-address="friend.email"
-        :is-favorite="friend.isFavorite"
-        @toggle-favorite="toggleFavoriteStatus"
-        @deletecontact="deleteContact"
-      ></friend-contact>
-    </ul>
-  </section>
+  <div>
+    <active-element
+      :topic-title="activeTopic && activeTopic.title"
+      :text="activeTopic && activeTopic.fullText"
+    ></active-element>
+    <knowledge-base></knowledge-base>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      friends: [],
+      topics: [
+        {
+          id: 'basics',
+          title: 'The Basics',
+          description: 'Core Vue basics you have to know',
+          fullText:
+            'Vue is a great framework and it has a couple of key concepts: Data binding, events, components and reactivity - that should tell you something!',
+        },
+        {
+          id: 'components',
+          title: 'Components',
+          description:
+            'Components are a core concept for building Vue UIs and apps',
+          fullText:
+            'With components, you can split logic (and markup) into separate building blocks and then combine those building blocks (and re-use them) to build powerful user interfaces.',
+        },
+      ],
+      activeTopic: null,
     };
   },
+
+  // Data that you would like to avail to another component directly without passing
+  // through another component(s)
+  // e.g. Provide topics directly to KnowledgeGrid without passing through
+  // KnowledgeBase component
+  provide() {
+    // Re-use the topics data inside provide to avoid duplication
+    return {
+      topics: this.topics,
+      // NB: Pass the function by reference 
+      // then invoke it where `selectTopic` is used
+      selectTopic: this.activateTopic,
+    };
+  },
+
   methods: {
-    toggleFavoriteStatus(friendId) {
-      let foundFriend = this.friends.find((friend) => friend.id === friendId);
-      // Negate that single found object's state
-      foundFriend.isFavorite = !foundFriend.isFavorite;
-    },
-    addContact({ name, phone, email }) {
-      const newFriendContact = {
-        id: new Date().toISOString(),
-        name,
-        phone,
-        email,
-      };
-      this.friends.push(newFriendContact);
-    },
-    deleteContact(id) {
-      // Overwrite existing array with new filtered results
-      this.friends = this.friends.filter((friend) => friend.id !== id);
+    activateTopic(topicId) {
+      this.activeTopic = this.topics.find((topic) => topic.id === topicId);
     },
   },
 };
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Jost&display=swap");
 * {
   box-sizing: border-box;
 }
-
 html {
-  font-family: "Jost", sans-serif;
+  font-family: sans-serif;
 }
-
 body {
   margin: 0;
 }
-
-header {
+section {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 3rem auto;
-  border-radius: 10px;
-  padding: 1rem;
-  background-color: #58004d;
-  color: white;
-  text-align: center;
-  width: 90%;
+  margin: 2rem auto;
   max-width: 40rem;
+  padding: 1rem;
+  border-radius: 12px;
 }
 
-#app ul {
+ul {
+  list-style: none;
   margin: 0;
   padding: 0;
-  list-style: none;
+  display: flex;
+  justify-content: center;
 }
 
-#app li,
-#app form {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  margin: 1rem auto;
-  border-radius: 10px;
+li {
+  border-radius: 12px;
+  border: 1px solid #ccc;
   padding: 1rem;
+  width: 15rem;
+  margin: 0 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+h2 {
+  margin: 0.75rem 0;
   text-align: center;
-  width: 90%;
-  max-width: 40rem;
 }
 
-#app h2 {
-  font-size: 2rem;
-  border-bottom: 4px solid #ccc;
-  color: #58004d;
-  margin: 0 0 1rem 0;
-}
-
-#app button {
+button {
   font: inherit;
-  cursor: pointer;
-  border: 1px solid #ff0077;
-  background-color: #ff0077;
+  border: 1px solid #c70053;
+  background-color: #c70053;
   color: white;
-  padding: 0.05rem 1rem;
-  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+  padding: 0.75rem 2rem;
+  border-radius: 30px;
+  cursor: pointer;
 }
 
-#app button:hover,
-#app button:active {
-  background-color: #ec3169;
-  border-color: #ec3169;
-  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
-}
-
-#app input {
-  font: inherit;
-  padding: 0.15rem;
-}
-#app label {
-  font-weight: bold;
-  margin-right: 1rem;
-  width: 7rem;
-  display: inline-block;
-}
-#app form div {
-  margin: 1rem 0;
+button:hover,
+button:active {
+  background-color: #e24d8b;
+  border-color: #e24d8b;
 }
 </style>
---
+++ src/components/KnowledgeGrid.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <knowledge-element
+      v-for="topic in topics"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
+    ></knowledge-element>
+  </ul>
+</template>
+
+<script>
+export default {
+  // Inject works like props but instead listens to what `provide` avails.
+  // NB: You can only use provide in a parent or ancestral component not inside
+  // a child
+  inject:['topics'],
+};
+</script>
