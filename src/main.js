34e82d32cde87d583678c6671256e8f56634adb8
David Mutua <mutuadavi@gmail.com>
2021-05-26 09:42:41 +0300

    feat(dive-deeper-into-components) Introduce slots

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 import BaseBadge from "./components/BaseBadge.vue";
+import BadgeCard from "./components/BadgeCard.vue";
 
 const app = createApp(App);
 
@@ -9,5 +10,8 @@ const app = createApp(App);
 // You can use them anywhere in the components
 // Used where a component is used in multiple places e.g. Masterpage components
 app.component("base-badge", BaseBadge);
+
+// Slot components are also global components
+app.component("badge-card", BadgeCard);
 
 app.mount("#app");
