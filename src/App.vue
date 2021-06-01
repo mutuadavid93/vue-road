28a18b6718763c48c97e3128730da0cb319f7956
David Mutua <mutuadavi@gmail.com>
2021-06-01 07:34:53 +0300

    feat(dive-deeper-into-components) Dynamic components

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,58 @@
 <template>
   <div>
     <the-header></the-header>
+    <!-- 
     <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
-    ></user-info>
+    ></user-info> -->
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent==='active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent==='manage-goals'"></manage-goals> -->
+
+    <!-- Instead use Dynamic components 
+      NB:: Pass your component name dynamically based on the passed name
+    -->
+    <component :is="selectedComponent"></component>
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import UserInfo from "./components/UserInfo.vue";
-import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import BadgeList from "./components/BadgeList.vue";
+
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
 
 export default {
   // Specify which components will be only used inside this component i.e. Local
   // components
   components: {
     "the-header": TheHeader,
-    "user-info": UserInfo,
-    "badge-list": BadgeList
+    // "user-info": UserInfo,
+    // "badge-list": BadgeList,
+    ActiveGoals,
+    ManageGoals,
   },
 
   data() {
     return {
+      selectedComponent:"active-goals",
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
         role: "admin",
       },
     };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
---
+++ src/components/ActiveGoals.vue
@@ -0,0 +1,5 @@
+<template>
+  <!-- NB:: Vue will auto-magically import components which lack 
+  default export -->
+  <h2>Active Goals</h2>
+</template>
