de052643a8d27db223b0d3667be1950fbc35361c
David Mutua <mutuadavi@gmail.com>
2021-06-09 20:26:28 +0300

    feat(use-vuex): Organize store using modules

--- src/main.js
+++ src/main.js
@@ -3,22 +3,13 @@ import { createStore } from 'vuex';
 
 import App from './App.vue';
 
-// NB :: We can only have one store per app
-// Registered here at main.js
-const store = createStore({
-  // state here is similar to data() in an individual component
+const counterModule = {
   state() {
     return {
-      counter: 0,
-      isLoggedIn: false
+      counter: 0
     };
   },
-
-  // Mutations :: Methods to change/update the state inside a store.
-  // Components reach out to mutations which do the magic updating state store
-  // NB :: Mutations are SYNCHRONOUS no async code!
   mutations: {
-    // @state is the current state
     increment(state) {
       // Update current state
       state.counter++;
@@ -27,15 +18,8 @@ const store = createStore({
     // @payload is the incoming dynamic data to update state
     increase(state, payload) {
       state.counter = state.counter + payload.value;
-    },
-
-    setAuth(state, payload) {
-      state.isLoggedIn = payload.isAuth;
     }
   },
-
-  // Getters :: similar to computed properties but read data from store into
-  // individual components
   getters: {
     finalCounter(state) {
       return state.counter * 2;
@@ -49,8 +33,50 @@ const store = createStore({
       if (finalcounter < 0) return 0;
       if (finalcounter >= 50) return 100 + '%';
       return finalcounter;
+    }
+  },
+  actions: {
+    increment(context) {
+      // Simulated async request
+      setTimeout(function() {
+        context.commit('increment'); // Invoke `increment` mutation
+      }, 2000);
     },
 
+    // Receaving forwarded payload from a component
+    increase(context, forwardedPayload) {
+      context.commit('increase', forwardedPayload);
+    }
+  }
+};
+
+// NB :: We can only have one store per app
+// Registered here at main.js
+const store = createStore({
+  // All modules will be merged into the root module
+  modules: {
+    numbers: counterModule
+  },
+  // state here is similar to data() in an individual component
+  state() {
+    return {
+      isLoggedIn: false
+    };
+  },
+
+  // Mutations :: Methods to change/update the state inside a store.
+  // Components reach out to mutations which do the magic updating state store
+  // NB :: Mutations are SYNCHRONOUS no async code!
+  mutations: {
+    // @state is the current state
+    setAuth(state, payload) {
+      state.isLoggedIn = payload.isAuth;
+    }
+  },
+
+  // Getters :: similar to computed properties but read data from store into
+  // individual components
+  getters: {
     isAuthenticated(state) {
       return state.isLoggedIn;
     }
@@ -62,18 +88,6 @@ const store = createStore({
     // Any ASYNC code should live inside below methods
     // Tip :: use same name as the target mutation. But isn't a must they match
     // @context contains juicy powers
-    increment(context) {
-      // Simulated async request
-      setTimeout(function() {
-        context.commit('increment'); // Invoke `increment` mutation
-      }, 2000);
-    },
-
-    // Receaving forwarded payload from a component
-    increase(context, forwardedPayload) {
-      context.commit('increase', forwardedPayload);
-    },
-
     login(context) {
       context.commit('setAuth', { isAuth: true });
     },
